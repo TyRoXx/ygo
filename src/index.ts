30fb56f73c45c43ac9149043e7eecc44161f63d8
TyRoXx <tyroxxxx@gmail.com>
2019-05-19 03:22:26 +0200

    render all zones

--- src/index.ts
+++ src/index.ts
@@ -1,49 +1,114 @@
-export function setUpBoard(): HTMLElement {
-    let cardHeight = 153
-    let upperRow = document.createElement("tr")
-    let lowerRow = document.createElement("tr")
-    let createCell = function (imageUrl: string) {
-        let cell = document.createElement("td")
-        let e = document.createElement("img")
-        e.setAttribute("src", imageUrl)
-        e.setAttribute("height", cardHeight.toString())
-        cell.appendChild(e)
-        return cell
+enum UpDownOrientation {
+    Up,
+    Down
+}
+
+const cardHeight = 153
+
+let createCell = function (imageUrl: string, playerOrientation: UpDownOrientation, defenseMode: boolean) {
+    let cell = document.createElement("td")
+    let image = document.createElement("img")
+    image.setAttribute("src", imageUrl)
+    image.setAttribute("height", cardHeight.toString())
+    cell.style.textAlign = "center"
+    cell.style.border = "1px solid black"
+    let rotation = (defenseMode ? 270 : 0)
+    switch (playerOrientation) {
+        case UpDownOrientation.Down:
+            rotation += 180
+            break
+        case UpDownOrientation.Up:
+            break
     }
+    image.style.transform = `rotate(${rotation}deg)`
+    cell.appendChild(image)
+    return cell
+}
+
+let setUpPlayer = function (board: HTMLTableElement, orientation: UpDownOrientation): void {
+    let appendChildren = function (to: HTMLElement, children: Array<HTMLElement>) {
+        switch (orientation) {
+            case UpDownOrientation.Down:
+                children = children.slice(0).reverse()
+                break;
+
+            case UpDownOrientation.Up:
+                break;
+        }
+        children.forEach(element => {
+            to.appendChild(element)
+        })
+    }
+    let upperRow = []
+    let lowerRow = []
+    upperRow.push(document.createElement("td"))
+    lowerRow.push(document.createElement("td"))
+
     let back = "https://vignette.wikia.nocookie.net/yugioh/images/e/e5/Back-EN.png/revision/latest?cb=20100726082133"
 
-    let fieldSpell = createCell(back)
-    upperRow.appendChild(fieldSpell)
+    let fieldSpell = createCell(back, orientation, false)
+    upperRow.push(fieldSpell)
 
-    let extraDeck = createCell(back)
-    lowerRow.appendChild(extraDeck)
+    let extraDeck = createCell(back, orientation, false)
+    lowerRow.push(extraDeck)
 
     for (let i = 0; i < 5; ++i) {
-        let monsterZone = createCell("https://ygoprodeck.com/pics/85936485.jpg")
-        upperRow.appendChild(monsterZone)
+        let monsterZone = createCell("https://ygoprodeck.com/pics/85936485.jpg", orientation, (i % 2) == 0)
+        upperRow.push(monsterZone)
         monsterZone.style.width = cardHeight.toString() + "px"
-        monsterZone.style.textAlign = "center"
-
-        if (i % 2) {
-            monsterZone.style.transform = "rotate(270deg)"
-        }
 
         let spellTrapZone = createCell((i % 2) ?
             back :
-            "https://ygoprodeck.com/pics/55144522.jpg")
-        lowerRow.appendChild(spellTrapZone)
+            "https://ygoprodeck.com/pics/55144522.jpg", orientation, false)
+        lowerRow.push(spellTrapZone)
         spellTrapZone.style.width = cardHeight.toString() + "px"
-        spellTrapZone.style.textAlign = "center"
     }
 
-    let graveyard = createCell("https://ygoprodeck.com/pics/85936485.jpg")
-    upperRow.appendChild(graveyard)
+    let graveyard = createCell("https://ygoprodeck.com/pics/85936485.jpg", orientation, false)
+    upperRow.push(graveyard)
 
-    let deck = createCell(back)
-    lowerRow.appendChild(deck)
+    let deck = createCell(back, orientation, false)
+    lowerRow.push(deck)
 
-    let player = document.createElement("table")
-    player.appendChild(upperRow)
-    player.appendChild(lowerRow)
-    return player
+    let banished = createCell("https://ygoprodeck.com/pics/85936485.jpg", orientation, false)
+    upperRow.push(banished)
+    lowerRow.push(document.createElement("td"))
+
+    let player = []
+    {
+        let tr = document.createElement("tr")
+        appendChildren(tr, upperRow)
+        player.push(tr)
+    }
+    {
+        let tr = document.createElement("tr")
+        appendChildren(tr, lowerRow)
+        player.push(tr)
+    }
+    appendChildren(board, player)
+}
+
+export function setUpBoard(): HTMLElement {
+    let board = document.createElement("table")
+    setUpPlayer(board, UpDownOrientation.Down)
+    {
+        let tr = document.createElement("tr")
+        for (let i = 0; i < 7; ++i) {
+            switch (i) {
+                case 3:
+                case 5:
+                    let extraMonsterZone = createCell("https://ygoprodeck.com/pics/23995346.jpg",
+                        (i == 3) ? UpDownOrientation.Down : UpDownOrientation.Up, false)
+                    tr.appendChild(extraMonsterZone)
+                    extraMonsterZone.style.width = cardHeight.toString() + "px"
+                    break
+
+                default:
+                    tr.appendChild(document.createElement("td"))
+            }
+        }
+        board.appendChild(tr)
+    }
+    setUpPlayer(board, UpDownOrientation.Up)
+    return board
 }
